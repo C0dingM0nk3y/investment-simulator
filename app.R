af9a3822d52518e815d687d46a4944f468d14371
CodingMonkey <reallycodingmonkey@gmail.com>
2023-07-29 23:32:29 +0200

    Update app.R

--- app.R
+++ app.R
@@ -9,6 +9,10 @@
 
 library(shiny)
 library(quantmod)
+library(magrittr)
+library(dplyr)
+library(stringr)
+library(lubridate)
 
 # 
 
@@ -21,7 +25,7 @@ ui <- fluidPage(
            h1("somehting catchy"),
            ),
     column(8,
-           h4("then maybe some description here?"),
+           h4("Chose one day in the past. Starting from that day, simualte the effecto of buying X amount of Y, every 30 days. How much would it be worth now?"),
            ),
   ),
 
@@ -34,6 +38,7 @@ ui <- fluidPage(
       sliderInput("years",
                   "How long ago should the investment start? (years)",
                   value = 10, min = 1, max = 40, step = 1, ticks = FALSE),
+      p(em("you do not know from which date to start? Try today, 10 years ago. Or your 25th birthday.")),
     ),
   ),
   
@@ -55,7 +60,7 @@ ui <- fluidPage(
       h3("Select Investment"),
     ),
     mainPanel(
-      textInput("asset_name",
+      textInput("symbol",
                 p("Chose one asset to invest into", br(), em("default: SPY = 'S&P500 full index'")), 
                 value = "SPY", placeholder = "any asset name supported by Yahoo Finance"),
       p(em("For a guide on Stock and Index names, refer to ", a("Yahoo Finance Symbol List", href="https://finance.yahoo.com/lookup/?guccounter=1", )),
@@ -68,15 +73,67 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  symbol <- "SPY"
+  startdate <- "1980-01-01" %>% as.Date()
+  inv_qnt <- 100
+  
+  simulateInv <- function(symbol, startdate, inv_qnt){
+    # DOWNLOAD LATEST DATA FROM YAHOO
+    xts <- getSymbols(symbol, 
+                       from = startdate,
+                       auto.assign = FALSE) #required to assign to custom variable name
+    
+    # CALCULATE AVERAGE PRICE 
+    # removes symbol name from df headers #eg. from SPY.Open to SPY
+    df <- data.frame(xts) #converts from xts object to standard df
+    colnames(df) %<>% str_remove(symbol)  %>% 
+      str_remove(".")
+    
+    df[,"avg_price"] <- (df$Open + df$Close)/2 # average price (mean of OPEN and CLOSE price)
+    
+    # CREATE SUBSET: 1 INVESTMENT EVERY 30 days
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #> Starting from startdate, subset table to only show prices on the days where a new investment was done.
+    #> NB: market is closed on SAT-SUN. Therefore the market data are missing all the rows corresponging to holidays.
+    #> For this reason, 1 investment every 30 days is converted into 1 investment every 20 work-days.
+    #> Table is filtered so to only keep one row every 20, starting from startdate
+    
+    df[,"daysFromStart"] <- 1:nrow(df)-1
+    df[,"filter"] <- ifelse(df$daysFromStart %% 20 ==0, TRUE, FALSE)
+    
+    ss <- subset(df, filter == TRUE, select = c("avg_price", "daysFromStart"))
+    
+    # SIMULATE PURCHASE
+    ss[,"value_inv"] <- inv_qnt
+    ss[,"buy_qnt"] <- with(ss, value_inv/avg_price)
+    
+    # cumulative calc
+    ss[,"cum_inv"] <- cumsum(ss$value_inv)
+    ss[,"cum_qnt"] <- cumsum(ss$buy_qnt)
+    
+    # current value of investment
+    latestPrice <- tail(df$avg_price, 1)
+    ss[,"cum_value"] <- ss$cum_qnt*latestPrice
+    
+    # ROI
+    ss[,"ratioTEMP"] <- with(ss, cum_value/cum_inv)
+    ss[,"ROI"] <- with(ss, (cum_value-cum_inv)/cum_inv)
+    
+    # STOPPED HERE
+    time <- difftime(tail(row.names(df), 1), head(row.names(df), 1), units = "weeks") %>%
+      as.numeric()/52 
+    finalRatio <- tail(ss$ratioTEMP, 1)
+    
+    # ORGANIZE RESULTS
+    res <- ss
+    
+    return(res)
+  }
+  
+  DF <- simulateInv(symbol, startdate, inv_qnt)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+
 }
 
 # Run the application 
